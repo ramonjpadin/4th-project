1393757ed1ea6779737232e2991f22d6da51990f
Ramon Padin <ramonjpadin@gmail.com>
2022-09-19 06:48:58 -0500

    some more java updates

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
 $('#currentDay').text(moment().format('[Today\'s Date: ]dddd, MMMM Do, YYYY h:mm:ss a'));
+
+$(function renderTextArea() {
+    let timeBlockRows = $('.row');
+    let currentTime = Number(moment().format('H'));
+
+    timeBlockRows.each(function (i) {
+        let rowText = $(this).children('textarea');
+        let storedText = JSON.parse(localStorage.getItem('row ' + i))
+
+        if (currentTime === $(this).children('textarea')) {
+
+        }
+    })
+}
